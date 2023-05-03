9f3246caa40bc9df4658e02d622e5cdf40185d5a
Carlos Barone <128315634+carbarcor@users.noreply.github.com>
2023-05-03 15:17:40 +0200

    3

--- BOKHYLLAN/website/__init__.py
+++ BOKHYLLAN/website/__init__.py
@@ -35,6 +35,8 @@ def create_app():
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+    login_manager.login_message = 'Du måste logga in för att komma åt din hemsida!'
+    
 
     @login_manager.user_loader
     def load_user(id):
